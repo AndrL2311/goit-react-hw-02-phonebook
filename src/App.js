ba81a1079009111513a04240746b59430bdb4645
Andrii Lypovetskyi <li_andr@ukr.net>
2021-10-02 19:43:15 +0300

    вытер ненужное

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import Profile from './components/profile/Profile';
-import user from './components/profile/user.json';
-import Statistics from './components/statistics/Statistics';
-import statisticalData from './components/statistics/statistical-data.json';
-import FriendList from './components/friendList/FriendList';
-import friendsData from './components/friendList/friends.json';
-import trasactionData from './components/transactions/transactions.json';
-import TransactionHistory from './components/transactions/TransactionHistory';
+import React from 'react';
 
-function App() {
-  return (
-    <div>
-      <Profile
-        name={user.name}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics title="Upload stats" stats={statisticalData} />
-      <FriendList friends={friendsData} />
-      <TransactionHistory items={trasactionData} />
-    </div>
-  );
+import s from './App.module.css';
+
+class App extends React.Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  render() {
+    return <div className={s.container}></div>;
+  }
 }
 
 export default App;
