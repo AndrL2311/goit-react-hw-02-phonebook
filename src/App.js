1a5953d7477b116e30bd0573897477f068e72230
Andrii Lypovetskyi <li_andr@ukr.net>
2021-10-02 22:33:55 +0300

    add component Form

--- src/App.js
+++ src/App.js
@@ -1,48 +1,17 @@
 import React from 'react';
 
 import s from './App.module.css';
+import Form from './components/Form/Form';
 
 class App extends React.Component {
-  state = {
-    contacts: [],
-    name: '',
-  };
-
-  handleChange = event => {
-    this.setState({ name: event.target.value });
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-    console.log(`Signed up as: ${this.state.name}`);
-
-    // Проп который передается форме для вызова при сабмите
-    // this.props.onSubmit(this.state.name);
+  formSubmitHandler = data => {
+    console.log(data);
   };
 
   render() {
-    const { name } = this.state;
-
     return (
       <div className={s.container}>
-        <h2>Phonebook</h2>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name
-            <input
-              type="text"
-              name="name"
-              value={name}
-              onChange={this.handleChange}
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-              required
-            />
-          </label>
-          <button className={s.button} type="submit">
-            Add Contact
-          </button>
-        </form>
+        <Form onSubmit={this.formSubmitHandler} />
       </div>
     );
   }
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+
+import s from './Form.module.css';
+
+class Form extends React.Component {
+  state = {
+    contacts: [],
+    name: '',
+    number: '',
+  };
+
+  handleChange = event => {
+    const { name, value } = event.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    // Проп который передается форме для вызова при сабмите
+    this.state.contacts.push({
+      id: 123,
+      name: this.state.name,
+      number: this.state.number,
+    });
+    this.props.onSubmit(this.state.contacts);
+    // Сбрасываем имя и номер
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    const { name, number } = this.state;
+
+    return (
+      <div className={s.container}>
+        <h2>Phonebook</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={this.handleChange}
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+              required
+            />
+          </label>
+          <label>
+            Number
+            <input
+              type="tel"
+              name="number"
+              value={number}
+              onChange={this.handleChange}
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+              required
+            />
+          </label>
+          <button className={s.button} type="submit">
+            Add Contact
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Form;
