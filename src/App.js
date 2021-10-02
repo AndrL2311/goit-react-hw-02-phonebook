8e91b5a42f493efc2addf699ee744004ae6c0ff1
Andrii Lypovetskyi <li_andr@ukr.net>
2021-10-02 20:04:50 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,8 +8,34 @@ class App extends React.Component {
     name: '',
   };
 
+  handleChange = e => {
+    this.setState({ login: e.target.name });
+  };
+
   render() {
-    return <div className={s.container}></div>;
+    const { name } = this.state;
+
+    return (
+      <div className={s.container}>
+        <h2>Phonebook</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name
+            <input
+              type="text"
+              name={name}
+              onChange={this.handleChange}
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+              required
+            />
+          </label>
+          <button className={s.button} type="submit">
+            Add Contact
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
