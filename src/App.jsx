5eec5ffea05b8d00f5a42eda9c71a9ae81cdc9ea
Andrii Lypovetskyi <li_andr@ukr.net>
2021-10-03 23:02:20 +0300

    contcts

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,16 @@ import { v4 as uuidv4 } from 'uuid';
 
 import s from './App.module.css';
 import Form from './components/Form/Form';
+import Contacts from './components/Contacts/Contacts';
 
 class App extends React.Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
   };
   formSubmitHandler = data => {
@@ -22,6 +28,7 @@ class App extends React.Component {
     return (
       <div className={s.container}>
         <Form onSubmit={this.formSubmitHandler} />
+        <Contacts contacts={this.state.contacts} />
       </div>
     );
   }
---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,37 @@
+import s from './Contacts.module.css';
+
+function Contacts({ contacts }) {
+  return (
+    <>
+      <h2>Contacts</h2>
+      <ul className={s.statistics}>
+        {contacts.map(contact => (
+          <li className="item" key={contact.id}>
+            <span className={s.label}>{contact.name}</span>
+            <span className={s.value}>{contact.number}</span>
+            <button
+              className={s.button}
+              type="button"
+              //   onClick={onLeaveFeedback}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default Contacts;
+
+//   contacts.length > 0 && contacts.map(contact => console.log(contact.id));}
+// (<li className="item">
+//         <span className={s.label}>Name</span>
+//             <span className={s.value}>Number</span>
+//             <button
+//           className={s.button}
+//           type="button"
+//         //   onClick={onLeaveFeedback}
+//         >Delete</button>
+//         </li>
